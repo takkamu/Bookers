c12e2be8d85060f226ad76f1fa4aefbb969ee584
EC2 Default User <ec2-user@ip-172-31-12-242.ec2.internal>
2022-05-12 12:32:05 +0000

    [ADD]投稿機能

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,5 +74,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  
+  config.hosts << "90b5fa1b81fb4271b1f4d65294be1c1a.vfs.cloud9.us-east-1.amazonaws.com"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'top' => 'homes#top'
+  get 'index' => 'books#index'
+  post 'books' => 'books#create'
+  get 'show' => 'books#show'
+  get 'edit' => 'books#edit'
 end
