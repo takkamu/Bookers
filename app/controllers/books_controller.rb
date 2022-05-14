66cb1fad9481ccab5a43f3aeb8cd6eb91b949886
EC2 Default User <ec2-user@ip-172-31-12-242.ec2.internal>
2022-05-14 06:05:46 +0000

    [Update]バリデーションとフラッシュメッセージの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,9 +5,19 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id), notice: 'Book was successfully created.'
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to '/books', notice: 'Book was successfully destroyed.'
   end
 
   def show
@@ -15,6 +25,17 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: 'Book was successfully updated.'
+    else
+      @books = Book.all
+      render :edit
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'top' => 'homes#top'
-  get 'index' => 'books#index'
+  get 'books' => 'books#index'
   post 'books' => 'books#create'
-  get 'books/:id' => 'books#show'
-  get 'edit' => 'books#edit', as: 'book'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 end
